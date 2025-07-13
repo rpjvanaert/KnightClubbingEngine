31430f2f5613bb67796789e8484405510f0cd82b
rpjvanaert <ralf@van-aert.nl>
2025-07-13 10:30:07 +0200

    Added determineMoveFromSan tests

--- src/test/java/knight/clubbing/opening/PgnParserTest.java
+++ src/test/java/knight/clubbing/opening/PgnParserTest.java
@@ -70,6 +70,216 @@ class PgnParserTest {
         assertFalse(result);
     }
 
+    @Test
+    void determineMoveFromSan_basic1() {
+        BBoard board = new BBoard("r2q1rk1/ppp1bppp/2n2n2/3p2B1/3P2b1/2N1PN2/PP2BPPP/R2Q1RK1 b - - 3 9");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "h6";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("h7"), BBoardHelper.stringCoordToIndex("h6"));
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_basic2() {
+        BBoard board = new BBoard("r4rk1/p1pqbppp/2pp1n2/4p1B1/4P1b1/2NP1N2/PPPQ1PPP/R4RK1 w - - 5 10");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "Ne1";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("f3"), BBoardHelper.stringCoordToIndex("e1"));
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_take1() {
+        BBoard board = new BBoard("r2q1rk1/ppp1bppp/2n2n2/3p2B1/3P2b1/2N1PN2/PP2BPPP/R2Q1RK1 b - - 3 9");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "Bxf3";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("g4"), BBoardHelper.stringCoordToIndex("f3"));
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_take2() {
+        BBoard board = new BBoard("r4rk1/p1pqbppp/2pp1n2/4p1B1/4P1b1/2NP1N2/PPPQ1PPP/R4RK1 w - - 5 10");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "Nxe5";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("f3"), BBoardHelper.stringCoordToIndex("e5"));
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_check1() {
+        BBoard board = new BBoard("rnbqkbnr/ppp1pppp/8/8/2pP4/8/PP2PPPP/RNBQKBNR w KQkq - 0 3");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "Qa4+";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("d1"), BBoardHelper.stringCoordToIndex("a4"));
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_check2() {
+        BBoard board = new BBoard("rnbqkbnr/pp2pppp/8/2Pp4/8/5N2/PPP1PPPP/RNBQKB1R b KQkq - 1 4");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "Qa5+";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("d8"), BBoardHelper.stringCoordToIndex("a5"));
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_castleShort() {
+        BBoard board = new BBoard("rnbqk2r/ppppbppp/5n2/4p3/4P3/5N2/PPPPBPPP/RNBQK2R w KQkq - 4 4");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "O-O";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("e1"), BBoardHelper.stringCoordToIndex("g1"), BMove.castleFlag);
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_castleLong() {
+        BBoard board = new BBoard("r3kbnr/ppp1qppp/2npb3/4p3/4P3/2NP1N2/PPP1BPPP/R1BQ1RK1 b kq - 0 6");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "O-O-O";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("e8"), BBoardHelper.stringCoordToIndex("c8"), BMove.castleFlag);
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_mate1() {
+        BBoard board = new BBoard("6k1/R7/1R6/8/8/3K4/8/8 w - - 0 1");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "Rb8#";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("b6"), BBoardHelper.stringCoordToIndex("b8"));
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_enPassant() {
+        BBoard board = new BBoard("rnbqkbnr/ppp1ppp1/7p/2Pp4/8/8/PP1PPPPP/RNBQKBNR w KQkq d6 0 3");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "cxd6";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("c5"), BBoardHelper.stringCoordToIndex("d6"), BMove.enPassantCaptureFlag);
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_promotionQueen() {
+        BBoard board = new BBoard("8/1P4k1/8/8/8/8/8/6K1 w - - 0 1");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "b8=Q";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("b7"), BBoardHelper.stringCoordToIndex("b8"), BMove.promoteToQueenFlag);
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_promotionRook() {
+        BBoard board = new BBoard("8/1P4k1/8/8/8/8/8/6K1 w - - 0 1");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "b8=R";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("b7"), BBoardHelper.stringCoordToIndex("b8"), BMove.promoteToRookFlag);
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_promotionBishop() {
+        BBoard board = new BBoard("8/1P4k1/8/8/8/8/8/6K1 w - - 0 1");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "b8=B";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("b7"), BBoardHelper.stringCoordToIndex("b8"), BMove.promoteToBishopFlag);
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_promotionKnight() {
+        BBoard board = new BBoard("8/1P4k1/8/8/8/8/8/6K1 w - - 0 1");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "b8=N";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("b7"), BBoardHelper.stringCoordToIndex("b8"), BMove.promoteToKnightFlag);
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void determineMoveFromSan_promotionQueen_check() {
+        BBoard board = new BBoard("6k1/1P6/8/8/8/8/8/6K1 w - - 0 1");
+        MoveGenerator moveGenerator = new MoveGenerator(board);
+        BMove[] possibleMoves = moveGenerator.generateMoves(false);
+        String san = "b8=Q+";
+        BMove expected = new BMove(BBoardHelper.stringCoordToIndex("b7"), BBoardHelper.stringCoordToIndex("b8"), BMove.promoteToQueenFlag);
+
+        BMove result = PgnParser.determineMoveFromSan(san, possibleMoves, board);
+
+        assertNotNull(result);
+        assertEquals(expected, result);
+    }
+
     //@Test
     void test() {
         String pgn = 
